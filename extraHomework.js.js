a83de48ce1b2130a2a5f7ab800913fed67dd5a8c
TShilongo <100943813+TShilongo@users.noreply.github.com>
2022-05-03 21:43:50 +0200

    Create extraHomework.js.js
    Console logged space for answers.

---
+++ extraHomework.js.js
@@ -0,0 +1,63 @@
+// Additional assignments for Day 3
+
+/* EXTRA 1
+ Use a ternary operator to assign to a variable called gender the string values "male" or "female".
+ The choice should be made based on the value of another variable called isMale.
+*/
+
+console.log("Extra 1")
+
+/* EXTRA 2
+ Write a piece of code for checking if, given two integers, the value of one of them is 8 or if their addition or subtraction is equal to 8.
+*/
+
+console.log("Extra 2")
+
+/* EXTRA 3
+ Create a variable and assign to it the concatenation of two strings.
+*/
+
+console.log("Extra 3")
+
+/* EXTRA 4
+ Create three variables and assign a numerical value to each one of them. 
+ Using a conditional statement, write a piece of code for sorting their values from highest to lowest.
+ Display the result in the console.
+*/
+
+console.log("Extra 4")
+
+/* EXTRA 5
+ Write a piece of code for finding the average of two given integers.
+*/
+
+console.log("Extra 5")
+
+/* EXTRA 6
+ Write a piece of code for finding the longest of two given strings.
+*/
+
+console.log("Extra 6")
+
+/* EXTRA 7
+ Write a piece of code for checking if a given value is a integer or not.
+*/
+
+console.log("Extra 7")
+
+/* EXTRA 8
+ Write a piece of code for calculating a certain percentage of a given number.
+ (Ex.: the 20% of 400 is 80)
+*/
+
+console.log("Extra 8")
+
+/* EXTRA 9
+ Write a piece of code for checking if a given number is even or odd.
+*/
+
+console.log("Extra 9")
+
+
+
+console.log("THE END")
